5a636eb7c33c7f10bd769b3848c765f40f0282fa
Aron Cutler <acutler84@gmail.com>
2017-01-31 19:32:39 -0800

    Complete group model

--- backend/app/models/group.rb
+++ backend/app/models/group.rb
@@ -1,2 +1,12 @@
 class Group < ActiveRecord::Base
+  validates :token, presence: true
+
+  belongs_to :tenant,
+  foreign_key: :tenant_id,
+  class_name: :User
+
+  belongs_to :landlord,
+  foreign_key: :landlord_id,
+  class_name: :User
+
 end
