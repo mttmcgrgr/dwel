6521c5816fc16b17cd795df6fcb1a81d1f712b83
Aron Cutler <acutler84@gmail.com>
2017-01-31 19:44:08 -0800

    Update Group and Todos models

--- backend/app/models/group.rb
+++ backend/app/models/group.rb
@@ -2,11 +2,14 @@ class Group < ActiveRecord::Base
   validates :token, presence: true
 
   belongs_to :tenant,
+  inverse_of: :groups,
   foreign_key: :tenant_id,
   class_name: :User
 
   belongs_to :landlord,
+  inverse_of: :groups,
   foreign_key: :landlord_id,
   class_name: :User
 
+  has_many :todos
 end
