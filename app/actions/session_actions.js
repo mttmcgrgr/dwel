ce0cb8d08328399308df50be0e670148f9519f2e
Victor Guillen <victorguillen@Victors-MacBook-Air.local>
2017-02-04 18:51:23 -0800

    commit

--- app/actions/session_actions.js
+++ app/actions/session_actions.js
@@ -31,3 +31,32 @@ export const receiveErrors = errors => ({
   type: RECEIVE_ERRORS,
   errors
 });
+
+import * as APIUtil from '../util/session_api_util';
+
+export const RECEIVE_USER = "RECEIVE_USER";
+export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
+
+export const receiveUser = responseData => ({
+  type: RECEIVE_USER,
+  responseData
+});
+
+export const receiveErrors = errors => ({
+  type: RECEIVE_ERRORS,
+  errors
+});
+
+export const signupUser = user => dispatch => (
+  APIUtil.registerUser(user)
+    .then((response) => response.json())
+    .then(responseData => dispatch(receiveUser(responseData)))
+    .catch(err => dispatch(receiveErrors(err.responseJSON)))
+);
+
+export const loginUser = user => dispatch => (
+  APIUtil.authenticateUser(user)
+    .then((response) => response.json())
+    .then(responseData => dispatch(receiveUser(responseData)))
+    .catch(err => dispatch(receiveErrors(err.responseJSON)))
+);
